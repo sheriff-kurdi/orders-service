506f83f33ae98e9df95c11e470c6ab36c1e36bae
kurdi <sheriff.kurdi@gmail.com>
2022-04-09 12:32:17 +0200

    repository

--- src/main/java/com/kurdi/ordersservice/repositories/OrdersRepository.java
+++ src/main/java/com/kurdi/ordersservice/repositories/OrdersRepository.java
@@ -4,4 +4,5 @@ import com.kurdi.ordersservice.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrdersRepository extends JpaRepository<Order, Integer> {
+    Order findByIdentity(Integer identity);
 }
