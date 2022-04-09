444ff5019c0121c73905b82bd0da0b4f78bd92ac
kurdi <sheriff.kurdi@gmail.com>
2022-04-09 12:31:59 +0200

    order item id

--- src/main/java/com/kurdi/ordersservice/entities/compositeKeys/OrderItemId.java
+++ src/main/java/com/kurdi/ordersservice/entities/compositeKeys/OrderItemId.java
@@ -1,6 +1,7 @@
 package com.kurdi.ordersservice.entities.compositeKeys;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public final class OrderItemId implements Serializable {
     // CartItem can be repeated(so if it repeated add the quantity rather than create new cart item)
      Integer identity;
